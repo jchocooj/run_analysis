22748c42e3c5051c6b949bb468e2ae09f8085f4f
Juan Arnoldo Chocooj I <47793480+jchocooj@users.noreply.github.com>
2019-05-06 14:40:16 -0600

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,59 @@
+#COURSERA- Getting and Cleaning Data Project,  by: Juan Arnoldo Chocooj Iten
+
+#1.Merges the training and the test sets to create one data set.
+#2.Extracts only the measurements on the mean and standard deviation for each measurement.
+#3.Uses descriptive activity names to name the activities in the data set
+#4.Appropriately labels the data set with descriptive variable names.
+#5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+#------Carga del archivo RAW-------i
+setwd("E:/R/Getting and Cleanning data/UCI HAR Dataset/")
+library(readr)
+library(dplyr)
+library(tidyr)
+
+#--Load de Headers
+features <- as.data.frame(read.table("features.txt", quote="\"", comment.char=""))
+activity_labels <- read.table("activity_labels.txt", quote="\"", comment.char="")
+
+#--Load de Test data
+subject_test <- read.table("test/subject_test.txt", quote="\"", comment.char="")
+X_test <- read.table("test/X_test.txt", quote="\"", comment.char="")
+y_test <- read.table("test/y_test.txt", quote="\"", comment.char="")
+
+#--Load the Trainning data
+subject_train <- read.table("train/subject_train.txt", quote="\"", comment.char="")
+X_train <- read.table("train/X_train.txt", quote="\"", comment.char="")
+y_train <- read.table("train/y_train.txt", quote="\"", comment.char="")
+
+#--Cleaning data
+features <- as.data.frame(t(features)) #Transpose data in columns to rows
+features <- slice(features, 2:2) #select only de row contain names
+features <- t(features)
+
+
+# 1  --Merging and combining data frames
+subject <- rbind(subject_test, subject_train)
+x <- rbind(X_test, X_train)
+y <- rbind(y_test, y_train)
+colnames(x) <- c(features)   #4.Appropriately labels the data set with descriptive variable names.
+colnames(y) <- c("Activity")
+colnames(subject) <- c("Subject")
+colnames(activity_labels) <- c("Activity","Act_Descrip")
+data <- cbind(subject,y,x)
+
+#2.Extracts only the measurements on the mean and standard deviation for each measurement.
+measurements <- data[,grepl("Subject|Activ|mean()|std()", colnames(data))]
+
+#3.Uses descriptive activity names to name the activities in the data set
+#library(plyr)
+measurements <- join(measurements, activity_labels, by= "Activity", type= "left", match= "first")
+measurements<- measurements[,c(2, 82, 3:81)]
+
+#5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+measure2 <- measurements[,c(3:81)]
+measurements_DF2 <- measurements%>%select(-Act_Descrip)%>%
+                    group_by(Subject)%>%
+                    summarise_each(funs(mean))
+  
+
